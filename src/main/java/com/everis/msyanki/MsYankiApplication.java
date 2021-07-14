422153f87d45ea12871157de8206c90e8513a83f
jhoss64gEveris <85947496+jhoss64gEveris@users.noreply.github.com>
2021-07-14 13:04:36 -0500

    initial commit

---
+++ src/main/java/com/everis/msyanki/MsYankiApplication.java
@@ -0,0 +1,13 @@
+package com.everis.msyanki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsYankiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsYankiApplication.class, args);
+	}
+
+}
